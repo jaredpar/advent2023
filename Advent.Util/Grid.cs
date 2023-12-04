b42a980f139a47be9bc538ea00d5aa749bc43e19
Jared Parsons <jared@paranoidcoding.org>
2023-12-03 16:55:01 -0800

    Day03 part2

---
+++ Advent.Util/Grid.cs
@@ -0,0 +1,75 @@
+﻿namespace Advent.Util;
+
+public sealed class Grid<T>
+{
+    private readonly T[] _items;
+    private readonly int _columns;
+
+    public int Rows => _items.Length / _columns;
+    public int Columns => _columns;
+
+    public Grid(int rows, int columns)
+    {
+        _items = new T[rows * columns];
+        _columns = columns;
+    }
+
+    public IEnumerable<(int Row, int Column)> GetAdjacentIndexes(int row, int column)
+    {
+        if (row - 1 >= 0)
+        {
+            yield return (row - 1, column);
+            if (column - 1 >= 0)
+                yield return (row - 1, column - 1);
+            if (column + 1 < Columns)
+                yield return (row - 1, column + 1);
+        }
+
+        if (row + 1 < Rows)
+        {
+            yield return (row + 1, column);
+            if (column - 1 >= 0)
+                yield return (row + 1, column - 1);
+            if (column + 1 < Columns)
+                yield return (row + 1, column + 1);
+        }
+
+        if (column - 1 >= 0)
+            yield return (row, column - 1);
+
+        if (column + 1 < Columns)
+            yield return (row, column + 1);
+    }
+
+    public ref T GetValue(int row, int column) => ref _items[(row * _columns) + column];
+
+    public Memory<T> GetRowMemory(int row) => _items.AsMemory(row * _columns, _columns);
+
+    public Span<T> GetRowSpan(int row) => _items.AsSpan(row * _columns, _columns);
+
+    public IEnumerable<(T, int row, int column)> GetValues()
+    {
+        for (int r = 0; r < Rows; r++)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                yield return (GetValue(r, c), r, c);
+            }
+        }
+    }
+
+    public static Grid<char> Parse(string[] lines)
+    {
+        var grid = new Grid<char>(lines.Length, lines[0].Length);
+        for (int r = 0; r < lines.Length; r++)
+        {
+            var line = lines[r];
+            for (int c = 0; c < line.Length; c++)
+            {
+                grid.GetValue(r, c) = line[c];
+            }
+        }
+
+        return grid;
+    }
+}
---
+++ Day03/Day03.cs
@@ -0,0 +1,77 @@
+﻿using Advent.Util;
+
+Part1("Input.txt");
+void Part1(string filePath)
+{
+    var schematic = new Schematic(filePath);
+    var sum = 0;
+    foreach (var number in schematic.GetPartNumbers())
+        sum += number;
+    Console.WriteLine(sum);
+}
+
+internal sealed class Schematic
+{
+    public string[] Lines { get; }
+    public Grid<char> Grid { get; }
+
+    public Schematic(string filePath)
+    {
+        Lines = File.ReadAllLines(filePath);
+        Grid = Grid<char>.Parse(Lines);
+    }
+
+    public bool IsNextToSymbol(int row, int column)
+    {
+        foreach (var (r, c) in Grid.GetAdjacentIndexes(row, column))
+        {
+            var item = Grid.GetValue(r, c);
+            if (!char.IsDigit(item) && item != '.')
+                return true;
+        }
+
+        return false;
+    }
+
+    public IEnumerable<int> GetPartNumbers()
+    {
+        foreach (var tuple in GetNumbers())
+        {
+            for (var i = 0; i < tuple.Memory.Length; i++)
+            {
+                if (IsNextToSymbol(tuple.Row, tuple.Column + i))
+                {
+                    yield return int.Parse(tuple.Memory.Span);
+                    break;
+                }
+            }
+        }
+    }
+
+    public IEnumerable<(Memory<char> Memory, int Row, int Column)> GetNumbers()
+    {
+        for (int r = 0; r < Grid.Rows; r++)
+        {
+            var memory = Grid.GetRowMemory(r);
+            var column = 0;
+            while (column < memory.Length)
+            {
+                if (!char.IsDigit(memory.Span[column]))
+                {
+                    column++;
+                    continue;
+                }
+
+                var start = column;
+                while (column < memory.Length && char.IsDigit(memory.Span[column]))
+                {
+                    column++;
+                }
+
+                var number = memory.Slice(start, column - start);
+                yield return (number, r, start);
+            }
+        }
+    }
+}
+
